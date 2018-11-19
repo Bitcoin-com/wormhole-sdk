1e63af72338851e379f48386608d7b47cd1ce678
Gabriel Cardona <gabriel@bitcoin.com>
2018-11-19 22:26:21 +0900

    Bump BITBOX SDK v

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const repl = require("repl")
 const Wormhole = require("./lib/Wormhole").default
 const clone = require("git-clone")
 
-program.version("1.4.1", "-v, --version")
+program.version("1.4.2", "-v, --version")
 
 program
   .command("new <name>")
